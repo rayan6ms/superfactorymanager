6d01a5694dc70525aa3e8765b59e89a218e3f171
TeamDman <TeamDman9201@gmail.com>
2025-01-18 14:09:36 -0500

    industrial foregoing test idk

--- src/gametest/java/ca/teamdman/sfm/gametest/compat/multiple/SFMIndustrialForegoingMekanismGameTests.java
+++ src/gametest/java/ca/teamdman/sfm/gametest/compat/multiple/SFMIndustrialForegoingMekanismGameTests.java
@@ -1,15 +1,23 @@
 package ca.teamdman.sfm.gametest.compat.multiple;
 
 import ca.teamdman.sfm.SFM;
+import ca.teamdman.sfm.common.blockentity.ManagerBlockEntity;
+import ca.teamdman.sfm.common.program.LabelPositionHolder;
+import ca.teamdman.sfm.common.registry.SFMBlocks;
+import ca.teamdman.sfm.common.registry.SFMItems;
+import ca.teamdman.sfm.gametest.SFMGameTestBase;
 import com.buuz135.industrial.block.resourceproduction.tile.WashingFactoryTile;
 import com.buuz135.industrial.module.ModuleCore;
 import com.buuz135.industrial.module.ModuleResourceProduction;
 import com.hrznstudio.titanium.block.tile.ActiveTile;
+import com.hrznstudio.titanium.client.screen.addon.FacingHandlerScreenAddon;
 import com.hrznstudio.titanium.component.fluid.MultiTankComponent;
 import com.hrznstudio.titanium.component.fluid.SidedFluidTankComponent;
 import com.hrznstudio.titanium.component.sideness.IFacingComponent;
 import com.hrznstudio.titanium.util.FacingUtil;
+import mekanism.api.fluid.IExtendedFluidTank;
 import mekanism.common.registries.MekanismBlocks;
+import mekanism.common.tile.TileEntityFluidTank;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.GameTest;
@@ -24,10 +32,11 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.gametest.GameTestHolder;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 @GameTestHolder(SFM.MOD_ID)
-public class SFMIndustrialForegoingMekanismGameTests {
+public class SFMIndustrialForegoingMekanismGameTests extends SFMGameTestBase {
     @SuppressWarnings("DanglingJavadoc")
     @GameTest(template = "3x4x3")
     public static void meat_fluid(GameTestHelper helper) {
@@ -39,6 +48,10 @@ public class SFMIndustrialForegoingMekanismGameTests {
         // set up the tanks
         helper.setBlock(leftTankPos, MekanismBlocks.BASIC_FLUID_TANK.getBlock());
         helper.setBlock(rightTankPos, MekanismBlocks.BASIC_FLUID_TANK.getBlock());
+        TileEntityFluidTank rightTank = (TileEntityFluidTank) helper.getBlockEntity(rightTankPos);
+        assert rightTank != null;
+        IExtendedFluidTank fluidTank = rightTank.getFluidTank(0, Direction.DOWN);
+        assert fluidTank != null;
 
         // set up the washing factory
         helper.setBlock(washingFactoryPos, ModuleResourceProduction.WASHING_FACTORY.getLeft().get());
@@ -47,8 +60,8 @@ public class SFMIndustrialForegoingMekanismGameTests {
 
         // configure it to push front
         /**
-         * {@link com.hrznstudio.titanium.client.screen.addon.FacingHandlerScreenAddon#mouseClicked}
-         * {@link com.hrznstudio.titanium.block.tile.ActiveTile#handleButtonMessage(int, Player, CompoundTag)}
+         * {@link FacingHandlerScreenAddon#mouseClicked}
+         * {@link ActiveTile#handleButtonMessage(int, Player, CompoundTag)}
          * {@link MultiTankComponent#handleFacingChange(String, FacingUtil.Sideness, int)}
          */
         CompoundTag click = new CompoundTag();
@@ -58,13 +71,15 @@ public class SFMIndustrialForegoingMekanismGameTests {
         try {
             Field multiTankComponentField = ActiveTile.class.getDeclaredField("multiTankComponent");
             multiTankComponentField.setAccessible(true);
-            MultiTankComponent<?> multiTankComponent = (MultiTankComponent<?>) multiTankComponentField.get(washingFactory);
+            MultiTankComponent<?> multiTankComponent = (MultiTankComponent<?>) multiTankComponentField.get(
+                    washingFactory);
             SidedFluidTankComponent<?> outputTank = (SidedFluidTankComponent<?>) multiTankComponent
                     .getTanks()
                     .stream()
                     .filter(tank -> tank.getName().equals("output"))
                     .findFirst()
                     .get();
+            outputTank.getFacingModes().put(FacingUtil.Sideness.BOTTOM, IFacingComponent.FaceMode.NONE);
             IFacingComponent.FaceMode[] validFacingModes = outputTank.getValidFacingModes();
             for (int i = 0; i < validFacingModes.length; i++) {
                 if (validFacingModes[i] == IFacingComponent.FaceMode.PUSH) {
@@ -101,6 +116,27 @@ public class SFMIndustrialForegoingMekanismGameTests {
                 .get()
                 .insertItem(0, new ItemStack(Items.RAW_IRON, 64), false);
 
-        helper.succeed();
+        // place the manager
+        helper.setBlock(managerPos, SFMBlocks.MANAGER_BLOCK.get());
+        ManagerBlockEntity manager = ((ManagerBlockEntity) helper.getBlockEntity(managerPos));
+        assert manager != null;
+        manager.setItem(0, new ItemStack(SFMItems.DISK_ITEM.get()));
+        manager.setProgram("""
+                                   NAME "bruh"
+                                   EVERY 20 TICKS DO
+                                    INPUT fluid:: FROM a
+                                    OUTPUT fluid:: TO b
+                                   END
+                                   """.stripTrailing().stripIndent());
+        LabelPositionHolder.empty()
+                .add("a", helper.absolutePos(leftTankPos))
+                .add("b", helper.absolutePos(rightTankPos))
+                .save(Objects.requireNonNull(manager.getDisk()));
+
+        helper.succeedWhen(() -> {
+            if (fluidTank.getFluidAmount() < 1000) {
+                helper.fail("Fluid tank did not receive fluid");
+            }
+        });
     }
 }
