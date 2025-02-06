51ea6217248eb11e0db9a95cbd317f2aa3736730
TeamDman <TeamDman9201@gmail.com>
2025-02-05 22:19:04 -0500

    fix crash loading form item extensions on server

--- src/main/java/ca/teamdman/sfm/client/render/FormItemExtensions.java
+++ src/main/java/ca/teamdman/sfm/client/render/FormItemExtensions.java
@@ -4,12 +4,13 @@ import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.common.registry.SFMItems;
 import ca.teamdman.sfm.common.util.MCVersionDependentBehaviour;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
-@EventBusSubscriber(modid= SFM.MOD_ID, bus= EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid= SFM.MOD_ID, bus= EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class FormItemExtensions implements IClientItemExtensions {
     private final BlockEntityWithoutLevelRenderer RENDERER = new FormItemRenderer();
 
