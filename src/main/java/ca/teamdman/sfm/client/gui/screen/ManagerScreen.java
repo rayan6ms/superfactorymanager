2305672507563d64f5f620947ee4a254dd4dc9f9
TeamDman <TeamDman9201@gmail.com>
2025-02-17 18:12:34 -0500

    merge

--- src/main/java/ca/teamdman/sfm/client/gui/screen/ManagerScreen.java
+++ src/main/java/ca/teamdman/sfm/client/gui/screen/ManagerScreen.java
@@ -636,11 +636,6 @@ public class ManagerScreen extends AbstractContainerScreen<ManagerContainerMenu>
         return 0F;
     }
 
-    @MCVersionDependentBehaviour
-    public float getBlitOffsetGood() {
-        return (float) getBlitOffset();
-    }
-
     @Override
     protected void renderTooltip(
             PoseStack pose,
