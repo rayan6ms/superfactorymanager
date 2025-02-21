14847a13bd5130e1899352a281457924bdcc4513
TeamDman <TeamDman9201@gmail.com>
2025-02-21 17:09:27 -0500

    Restore mek linting for 1.21.1

--- src/main/java/ca/teamdman/sfm/common/program/linting/ProgramLinter.java
+++ src/main/java/ca/teamdman/sfm/common/program/linting/ProgramLinter.java
@@ -3,16 +3,22 @@ package ca.teamdman.sfm.common.program.linting;
 import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.common.blockentity.ManagerBlockEntity;
 import ca.teamdman.sfm.common.cablenetwork.CableNetworkManager;
+import ca.teamdman.sfm.common.compat.SFMMekanismCompat;
+import ca.teamdman.sfm.common.compat.SFMModCompat;
 import ca.teamdman.sfm.common.item.DiskItem;
 import ca.teamdman.sfm.common.program.LabelPositionHolder;
 import ca.teamdman.sfm.common.program.ProgramContext;
 import ca.teamdman.sfm.common.registry.SFMBlockCapabilities;
-import ca.teamdman.sfm.common.registry.SFMResourceTypes;
-import ca.teamdman.sfm.common.util.SFMDirections;
-import ca.teamdman.sfml.ast.IOStatement;
-import ca.teamdman.sfml.ast.Program;
-import ca.teamdman.sfml.ast.ResourceQuantity;
-import ca.teamdman.sfml.ast.RoundRobin;
+import ca.teamdman.sfm.common.util.MCVersionDependentBehaviour;
+import ca.teamdman.sfml.ast.*;
+import com.mojang.datafixers.util.Pair;
+import mekanism.api.RelativeSide;
+import mekanism.common.lib.transmitter.TransmissionType;
+import mekanism.common.tile.component.TileComponentConfig;
+import mekanism.common.tile.component.config.ConfigInfo;
+import mekanism.common.tile.component.config.DataType;
+import mekanism.common.tile.interfaces.ISideConfiguration;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.resources.ResourceLocation;
@@ -21,8 +27,10 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static ca.teamdman.sfm.common.localization.LocalizationKeys.*;
 import static ca.teamdman.sfml.ast.RoundRobin.Behaviour.BY_BLOCK;
@@ -123,67 +131,71 @@ public class ProgramLinter {
             LabelPositionHolder labelPositionHolder,
             Level level
     ) {
-//        if (!SFMModCompat.isMekanismLoaded()) return;
-//        DirectionQualifier directions = statement.labelAccess().directions();
-//        Stream<Pair<Label, BlockPos>> mekanismBlocks = statement
-//                .labelAccess()
-//                .getLabelledPositions(labelPositionHolder)
-//                .stream()
-//                .filter(pair -> level.isLoaded(pair.getSecond()))
-//                .filter(pair -> SFMModCompat.isMekanismBlock(level, pair.getSecond()));
-//
-//        // add warning if interacting with mekanism but the mekanism side config is not ALLOW
-//        EnumSet<TransmissionType> referencedTransmissionTypes = SFMMekanismCompat
-//                .getReferencedTransmissionTypes(statement);
-//        Predicate<DataType> dataTypePredicate;
-//        DataType fixed;
-//        if (statement instanceof InputStatement) {
-//            dataTypePredicate = DataType::canOutput;
-//            fixed = DataType.OUTPUT; // to input from it, it must be set to output
-//        } else if (statement instanceof OutputStatement) {
-//            dataTypePredicate = dataType -> dataType == DataType.INPUT
-//                                            || dataType == DataType.INPUT_OUTPUT
-//                                            || dataType == DataType.INPUT_1
-//                                            || dataType == DataType.INPUT_2;
-//            fixed = DataType.INPUT; // to output from it, it must be set to input
-//        } else {
-//            throw new IllegalStateException("Unexpected value: " + statement);
-//        }
-//        mekanismBlocks.forEach(pair -> {
-//            BlockPos blockPos = pair.getSecond();
-//            if (level.getBlockEntity(blockPos) instanceof ISideConfiguration mekBlockEntity) {
-//                TileComponentConfig mekBlockEntityConfig = mekBlockEntity.getConfig();
-//                for (TransmissionType transmissionType : referencedTransmissionTypes) {
-//                    boolean anySuccess = false;
-//                    ConfigInfo transmissionConfig = mekBlockEntityConfig.getConfig(transmissionType);
-//                    if (transmissionConfig != null) {
-//                        Set<Direction> activeSides = transmissionConfig.getSides(dataTypePredicate);
-//                        for (Direction direction : directions) {
-//                            if (activeSides.contains(direction)) {
-//                                anySuccess = true;
-//                                break;
-//                            }
-//                        }
-//                        if (!anySuccess) {
-//                            // we want to enable the side for the transmission type
-//                            // pick the first direction in the statement
-//                            Direction statementSide = directions.iterator().next();
-//                            if (statementSide != null) {
-//                                RelativeSide relativeSide = RelativeSide.fromDirections(
-//                                        mekBlockEntity.getDirection(),
-//                                        statementSide
-//                                );
-//                                transmissionConfig.setDataType(
-//                                        fixed,
-//                                        relativeSide
-//                                );
-//                                mekBlockEntityConfig.sideChanged(transmissionType, relativeSide);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        });
+        if (!SFMModCompat.isMekanismLoaded()) return;
+        DirectionQualifier directions = statement.labelAccess().directions();
+        Stream<Pair<Label, BlockPos>> mekanismBlocks = statement
+                .labelAccess()
+                .getLabelledPositions(labelPositionHolder)
+                .stream()
+                .filter(pair -> level.isLoaded(pair.getSecond()))
+                .filter(pair -> SFMModCompat.isMekanismBlock(level, pair.getSecond()));
+
+        // add warning if interacting with mekanism but the mekanism side config is not ALLOW
+        EnumSet<TransmissionType> referencedTransmissionTypes = SFMMekanismCompat
+                .getReferencedTransmissionTypes(statement);
+        Predicate<DataType> dataTypePredicate;
+        DataType fixed;
+        if (statement instanceof InputStatement) {
+            dataTypePredicate = DataType::canOutput;
+            fixed = DataType.OUTPUT; // to input from it, it must be set to output
+        } else if (statement instanceof OutputStatement) {
+            dataTypePredicate = dataType -> dataType == DataType.INPUT
+                                            || dataType == DataType.INPUT_OUTPUT
+                                            || dataType == DataType.INPUT_1
+                                            || dataType == DataType.INPUT_2;
+            fixed = DataType.INPUT; // to output from it, it must be set to input
+        } else {
+            throw new IllegalStateException("Unexpected value: " + statement);
+        }
+        mekanismBlocks.forEach(pair -> {
+            BlockPos blockPos = pair.getSecond();
+            if (level.getBlockEntity(blockPos) instanceof ISideConfiguration mekBlockEntity) {
+                TileComponentConfig mekBlockEntityConfig = mekBlockEntity.getConfig();
+                for (TransmissionType transmissionType : referencedTransmissionTypes) {
+                    boolean anySuccess = false;
+                    ConfigInfo transmissionConfig = mekBlockEntityConfig.getConfig(transmissionType);
+                    if (transmissionConfig != null) {
+                        Set<Direction> activeSides = getSideConfig(
+                                transmissionConfig,
+                                dataTypePredicate,
+                                mekBlockEntity.getDirection()
+                        );
+                        for (Direction direction : directions) {
+                            if (activeSides.contains(direction)) {
+                                anySuccess = true;
+                                break;
+                            }
+                        }
+                        if (!anySuccess) {
+                            // we want to enable the side for the transmission type
+                            // pick the first direction in the statement
+                            Direction statementSide = directions.iterator().next();
+                            if (statementSide != null) {
+                                RelativeSide relativeSide = RelativeSide.fromDirections(
+                                        mekBlockEntity.getDirection(),
+                                        statementSide
+                                );
+                                transmissionConfig.setDataType(
+                                        fixed,
+                                        relativeSide
+                                );
+                                mekBlockEntityConfig.sideChanged(transmissionType, relativeSide);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 
     private static void addWarningsForSmellyMekanismAccess(
@@ -193,64 +205,84 @@ public class ProgramLinter {
             Level level,
             ArrayList<TranslatableContents> warnings
     ) {
-//        if (!SFMModCompat.isMekanismLoaded()) return;
-//        DirectionQualifier directions = statement.labelAccess().directions();
-//        Stream<Pair<Label, BlockPos>> mekanismBlocks = statement
-//                .labelAccess()
-//                .getLabelledPositions(labelPositionHolder)
-//                .stream()
-//                .filter(pair -> level.isLoaded(pair.getSecond()))
-//                .filter(pair -> SFMModCompat.isMekanismBlock(level, pair.getSecond()));
-//        if (directions.equals(DirectionQualifier.NULL_DIRECTION)) {
-//            // add warning if interacting with mekanism without specifying a side
-//            // are any of the blocks mekanism?
-//            mekanismBlocks
-//                    .forEach(pair -> warnings.add(PROGRAM_WARNING_MEKANISM_USED_WITHOUT_DIRECTION.get(
-//                            pair.getFirst(),
-//                            statement.toStringPretty()
-//                    )));
-//        } else {
-//            // add warning if interacting with mekanism but the mekanism side config is not ALLOW
-//            EnumSet<TransmissionType> referencedTransmissionTypes = SFMMekanismCompat
-//                    .getReferencedTransmissionTypes(statement);
-//            Predicate<DataType> dataTypePredicate;
-//            if (ioStatement instanceof InputStatement) {
-//                dataTypePredicate = DataType::canOutput;
-//            } else if (ioStatement instanceof OutputStatement) {
-//                dataTypePredicate = dataType -> dataType == DataType.INPUT
-//                                                || dataType == DataType.INPUT_OUTPUT
-//                                                || dataType == DataType.INPUT_1
-//                                                || dataType == DataType.INPUT_2;
-//            } else {
-//                throw new IllegalStateException("Unexpected value: " + ioStatement);
-//            }
-//            mekanismBlocks.forEach(pair -> {
-//                BlockPos blockPos = pair.getSecond();
-//                if (level.getBlockEntity(blockPos) instanceof ISideConfiguration mekBlockEntity) {
-//                    TileComponentConfig config = mekBlockEntity.getConfig();
-//                    for (TransmissionType transmissionType : referencedTransmissionTypes) {
-//                        boolean anySuccess = false;
-//                        ConfigInfo transmissionConfig = config.getConfig(transmissionType);
-//                        if (transmissionConfig != null) {
-//                            Set<Direction> activeSides = transmissionConfig.getSides(dataTypePredicate);
-//                            for (Direction direction : directions) {
-//                                if (activeSides.contains(direction)) {
-//                                    anySuccess = true;
-//                                    break;
-//                                }
-//                            }
-//                        }
-//                        if (!anySuccess) {
-//                            warnings.add(PROGRAM_WARNING_MEKANISM_BAD_SIDE_CONFIG.get(
-//                                    blockPos,
-//                                    pair.getFirst(),
-//                                    statement.toStringPretty()
-//                            ));
-//                        }
-//                    }
-//                }
-//            });
-//        }
+        if (!SFMModCompat.isMekanismLoaded()) return;
+        DirectionQualifier directions = statement.labelAccess().directions();
+        Stream<Pair<Label, BlockPos>> mekanismBlocks = statement
+                .labelAccess()
+                .getLabelledPositions(labelPositionHolder)
+                .stream()
+                .filter(pair -> level.isLoaded(pair.getSecond()))
+                .filter(pair -> SFMModCompat.isMekanismBlock(level, pair.getSecond()));
+        if (directions.equals(DirectionQualifier.NULL_DIRECTION)) {
+            // add warning if interacting with mekanism without specifying a side
+            // are any of the blocks mekanism?
+            mekanismBlocks
+                    .forEach(pair -> warnings.add(PROGRAM_WARNING_MEKANISM_USED_WITHOUT_DIRECTION.get(
+                            pair.getFirst(),
+                            statement.toStringPretty()
+                    )));
+        } else {
+            // add warning if interacting with mekanism but the mekanism side config is not ALLOW
+            EnumSet<TransmissionType> referencedTransmissionTypes = SFMMekanismCompat
+                    .getReferencedTransmissionTypes(statement);
+            Predicate<DataType> dataTypePredicate;
+            if (ioStatement instanceof InputStatement) {
+                dataTypePredicate = DataType::canOutput;
+            } else if (ioStatement instanceof OutputStatement) {
+                dataTypePredicate = dataType -> dataType == DataType.INPUT
+                                                || dataType == DataType.INPUT_OUTPUT
+                                                || dataType == DataType.INPUT_1
+                                                || dataType == DataType.INPUT_2;
+            } else {
+                throw new IllegalStateException("Unexpected value: " + ioStatement);
+            }
+            mekanismBlocks.forEach(pair -> {
+                BlockPos blockPos = pair.getSecond();
+                if (level.getBlockEntity(blockPos) instanceof ISideConfiguration mekBlockEntity) {
+                    TileComponentConfig config = mekBlockEntity.getConfig();
+                    for (TransmissionType transmissionType : referencedTransmissionTypes) {
+                        boolean anySuccess = false;
+                        ConfigInfo transmissionConfig = config.getConfig(transmissionType);
+                        if (transmissionConfig != null) {
+                            Set<Direction> activeSides = getSideConfig(
+                                    transmissionConfig,
+                                    dataTypePredicate,
+                                    mekBlockEntity.getDirection()
+                            );
+                            for (Direction direction : directions) {
+                                if (activeSides.contains(direction)) {
+                                    anySuccess = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (!anySuccess) {
+                            warnings.add(PROGRAM_WARNING_MEKANISM_BAD_SIDE_CONFIG.get(
+                                    blockPos,
+                                    pair.getFirst(),
+                                    statement.toStringPretty()
+                            ));
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @MCVersionDependentBehaviour
+    private static Set<Direction> getSideConfig(
+            ConfigInfo transmissionConfig,
+            Predicate<DataType> dataTypePredicate,
+            Direction blockFacing
+    ) {
+        Set<Map.Entry<RelativeSide, DataType>> sideConfig = transmissionConfig.getSideConfig();
+        Set<RelativeSide> found = new HashSet<>();
+        for (var x : sideConfig) {
+            if (dataTypePredicate.test(x.getValue())) {
+                found.add(x.getKey());
+            }
+        }
+        return found.stream().map(x -> x.getDirection(blockFacing)).collect(Collectors.toSet());
     }
 
     private static void addWarningsForUsingIOWithoutCorrespondingOppositeIO(
