61f798dc67b1d224ffcf89b2add2d41424668fa5
TeamDman <TeamDman9201@gmail.com>
2025-04-14 18:14:57 -0400

    Add test for name matching, fix comma error

--- src/gametest/java/ca/teamdman/sfm/gametest/SFMNBTGameTests.java
+++ src/gametest/java/ca/teamdman/sfm/gametest/SFMNBTGameTests.java
@@ -3,6 +3,7 @@ package ca.teamdman.sfm.gametest;
 import ca.teamdman.sfm.SFM;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.gametest.GameTestHolder;
@@ -15,12 +16,12 @@ import java.util.Arrays;
         "deprecation",
         "OptionalGetWithoutIsPresent",
         "DuplicatedCode",
-        , "ArraysAsListWithZeroOrOneArgument"
+        "ArraysAsListWithZeroOrOneArgument"
 })
 @GameTestHolder(SFM.MOD_ID)
 public class SFMNBTGameTests extends SFMGameTestBase {
     @GameTest(template = "3x2x1")
-    public static void nbt_name(GameTestHelper helper) {
+    public static void nbt_damage(GameTestHelper helper) {
         var pickaxe = new ItemStack(Items.DIAMOND_PICKAXE);
         var pickaxeWithDamage = pickaxe.copy();
         pickaxeWithDamage.setDamageValue(12);
@@ -37,10 +38,36 @@ public class SFMNBTGameTests extends SFMGameTestBase {
                         pickaxeWithDamage.copy()
                 ))
                 .postContents("left", Arrays.asList(
-                        pickaxe.copy()
+                        pickaxe
                 ))
                 .postContents("right", Arrays.asList(
-                        pickaxeWithDamage.copy()
+                        pickaxeWithDamage
+                ))
+                .run();
+    }
+
+    @GameTest(template = "3x2x1")
+    public static void nbt_name(GameTestHelper helper) {
+        var stick = new ItemStack(Items.STICK, 64);
+        var stickWithName = stick.copy();
+        stickWithName.setHoverName(Component.literal("brah"));
+
+        new LeftRightManagerTest(helper)
+                .setProgram("""
+                                    EVERY 20 TICKS DO
+                                      INPUT WITH NBT {display:{Name:'[{"text":"bruh"}]'}} FROM left
+                                      OUTPUT TO right
+                                    END
+                                    """)
+                .preContents("left", Arrays.asList(
+                        stickWithName.copy(),
+                        stick.copy()
+                ))
+                .postContents("left", Arrays.asList(
+                        stick
+                ))
+                .postContents("right", Arrays.asList(
+                        stickWithName
                 ))
                 .run();
     }
