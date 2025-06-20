9e77fde5aece8cd268e12c01cd0d3b07d690d0ba
TeamDman <TeamDman9201@gmail.com>
2025-06-20 17:41:04 -0400

    Fix "/ by zero exception" in program edit screen, fixes #307

--- src/main/java/ca/teamdman/sfm/client/gui/screen/ProgramEditorScreen.java
+++ src/main/java/ca/teamdman/sfm/client/gui/screen/ProgramEditorScreen.java
@@ -477,6 +477,11 @@ public class ProgramEditorScreen extends Screen {
         }
 
         @Override
+        protected int getMaxScrollAmount() {
+            return Math.max(1, super.getMaxScrollAmount()); // Fix #307: divide by zero exception
+        }
+
+        @Override
         public boolean mouseDragged(
                 double mx,
                 double my,
@@ -489,6 +494,12 @@ public class ProgramEditorScreen extends Screen {
             if (mx >= thisX + 1 && mx <= thisX + this.width - 1) {
                 mx -= getLineNumberWidth();
             }
+
+            // Fix for division by zero error in AbstractScrollWidget.mouseDragged
+            if (this.height == this.getScrollBarHeight()) {
+                return false;
+            }
+
             return super.mouseDragged(mx, my, button, dx, dy);
         }
 
@@ -697,4 +708,3 @@ public class ProgramEditorScreen extends Screen {
         }
     }
 }
-
