522f6a13bef851fb2ebe9ff401977df3d77ae7df
TeamDman <TeamDman9201@gmail.com>
2025-07-06 12:17:32 -0400

    Move mixins to main source set
    This will increase bundle size by a small amount for code that is never used in release, but it's better than the error-level log message form the mixin missing :/

--- src/main/java/ca/teamdman/sfm/mixins/StructureTemplateManagerMixin.java
+++ src/main/java/ca/teamdman/sfm/mixins/StructureTemplateManagerMixin.java
@@ -6,13 +6,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-@SuppressWarnings("UnusedMixin") // this is because we have to have the stupid config in the main source set
 @Mixin(value = StructureTemplateManager.class, priority = 0)
 public class StructureTemplateManagerMixin {
 //    @Inject(method = "tryLoad", at = @At("HEAD"), cancellable = true)
