95820faeb71a46ed4975d85a835aa6648c194a84
TeamDman <TeamDman9201@gmail.com>
2025-09-11 00:27:40 -0400

    Add clarity to changelog.sfml for SFMBlockTags.java

--- src/main/java/ca/teamdman/sfm/client/registry/SFMBlockTags.java
+++ src/main/java/ca/teamdman/sfm/client/registry/SFMBlockTags.java
@@ -5,9 +5,13 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
+///  See also: {@link ca.teamdman.sfm.datagen.SFMBlockTagsDatagen}
 public class SFMBlockTags {
+    ///  What the anvil must land on for disenchanting to work, traditionally obsidian
     public static final TagKey<Block> ANVIL_DISENCHANTING = BlockTags.create(
             SFMResourceLocation.fromSFMPath("anvil_disenchanting"));
+
+    ///  What the anvil must land on to turn the block into a form, traditionally a metal block
     public static final TagKey<Block> ANVIL_PRINTING_PRESS_FORMING = BlockTags.create(
             SFMResourceLocation.fromSFMPath("anvil_printing_press_forming"));
 
