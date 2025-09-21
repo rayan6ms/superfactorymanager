0a8b377d5d07d51f33f1f842afd37a6af5b76144
TeamDman <TeamDman9201@gmail.com>
2025-09-21 12:55:59 -0400

    Clean up creative tab display name

--- src/main/java/ca/teamdman/sfm/common/registry/SFMCreativeTabs.java
+++ src/main/java/ca/teamdman/sfm/common/registry/SFMCreativeTabs.java
@@ -1,6 +1,7 @@
 package ca.teamdman.sfm.common.registry;
 
 import ca.teamdman.sfm.SFM;
+import ca.teamdman.sfm.common.localization.LocalizationKeys;
 import ca.teamdman.sfm.common.util.MCVersionDependentBehaviour;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -11,8 +12,6 @@ public class SFMCreativeTabs {
     public static final CreativeModeTab TAB = new SFMCreativeModeTab();
 
     public static class SFMCreativeModeTab extends CreativeModeTab {
-        public static final String DISPLAY_NAME_TRANSLATION_KEY = "item_group." + SFM.MOD_ID;
-        public static final Component DISPLAY_NAME = Component.translatable(DISPLAY_NAME_TRANSLATION_KEY);
         public SFMCreativeModeTab() {
             super(SFM.MOD_ID);
         }
@@ -24,7 +23,7 @@ public class SFMCreativeTabs {
 
         @Override
         public Component getDisplayName() {
-            return DISPLAY_NAME;
+            return LocalizationKeys.CREATIVE_TAB.getComponent();
         }
     }
 }
