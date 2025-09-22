4a388834703be0451ef19027ead20db927e32b43
TeamDman <TeamDman9201@gmail.com>
2025-09-21 21:15:36 -0400

    Move CapabilityDiscoveryMapperGameTest.java

--- src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/multiple/CapabilityDiscoveryMapperGameTest.java
+++ src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/multiple/CapabilityDiscoveryMapperGameTest.java
@@ -1,4 +1,4 @@
-package ca.teamdman.sfm.gametest.tests.compat.mekanism;
+package ca.teamdman.sfm.gametest.tests.compat.multiple;
 
 import ca.teamdman.sfm.common.capability.SFMBlockCapabilityDiscovery;
 import ca.teamdman.sfm.common.capability.SFMWellKnownCapabilities;
