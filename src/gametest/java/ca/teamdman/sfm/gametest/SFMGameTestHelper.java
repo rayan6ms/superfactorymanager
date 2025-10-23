68a0e68f27732a73fbc1c3d0154db3935e72821d
TeamDman <TeamDman9201@gmail.com>
2025-10-22 22:51:45 -0400

    Undo c0c1610bb3774385ccae9cfdb5f81f75c14972b3 lol

--- src/gametest/java/ca/teamdman/sfm/gametest/SFMGameTestHelper.java
+++ src/gametest/java/ca/teamdman/sfm/gametest/SFMGameTestHelper.java
@@ -28,13 +28,15 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.LongStream;
 
 public class SFMGameTestHelper extends GameTestHelper {
     public SFMGameTestHelper(GameTestInfo pTestInfo) {
+
         super(pTestInfo);
     }
+
     public SFMGameTestHelper(GameTestHelper helper) {
+
         super(helper.testInfo);
     }
 
@@ -43,6 +45,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             @NotStored BlockPos localPos,
             @Nullable Direction direction
     ) {
+
         SFMBlockCapabilityResult<CAP> found = SFMBlockCapabilityDiscovery.discoverCapabilityFromLevel(
                 getLevel(),
                 capKind,
@@ -57,6 +60,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             @NotStored BlockPos pos,
             @Nullable Direction direction
     ) {
+
         return discoverCapability(
                 SFMWellKnownCapabilities.FLUID_HANDLER,
                 pos,
@@ -68,6 +72,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             @NotStored BlockPos pos,
             @Nullable Direction direction
     ) {
+
         return discoverCapability(
                 SFMWellKnownCapabilities.ITEM_HANDLER,
                 pos,
@@ -79,6 +84,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             BlockPos signPos,
             Component... text
     ) {
+
         BlockEntity blockEntity = getBlockEntity(signPos);
         if (!(blockEntity instanceof SignBlockEntity signBlockEntity)) {
             fail("Block entity was not an instance of SignBlockEntity, got " + blockEntity, signPos);
@@ -97,6 +103,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             @NotStored BlockPos pos,
             @Nullable Direction direction
     ) {
+
         return discoverCapability(
                 SFMWellKnownCapabilities.ENERGY,
                 pos,
@@ -107,6 +114,7 @@ public class SFMGameTestHelper extends GameTestHelper {
     public IItemHandler getItemHandler(
             @NotStored BlockPos pos
     ) {
+
         return getItemHandler(pos, null);
     }
 
@@ -115,6 +123,7 @@ public class SFMGameTestHelper extends GameTestHelper {
             Runnable assertion,
             Runnable onSuccess
     ) {
+
         SFMGameTestMethodHelpers.assertManagerRunning(manager); // the program should already be compiled so we can monkey patch it
         manager.enableRebuildProgramLock();
         var hasExecuted = new AtomicBoolean(false);
@@ -127,6 +136,7 @@ public class SFMGameTestHelper extends GameTestHelper {
         Trigger startTimerTrigger = new Trigger() {
             @Override
             public boolean shouldTick(ProgramContext context) {
+
                 return oldFirstTrigger != null
                        ? oldFirstTrigger.shouldTick(context)
                        : context.getManager().getTick() % 20 == 0;
@@ -134,11 +144,13 @@ public class SFMGameTestHelper extends GameTestHelper {
 
             @Override
             public void tick(ProgramContext context) {
+
                 startTime.set(System.nanoTime());
             }
 
             @Override
             public Block getBlock() {
+
                 return new Block(Collections.emptyList());
             }
         };
@@ -146,6 +158,7 @@ public class SFMGameTestHelper extends GameTestHelper {
         Trigger endTimerTrigger = new Trigger() {
             @Override
             public boolean shouldTick(ProgramContext context) {
+
                 return oldFirstTrigger != null
                        ? oldFirstTrigger.shouldTick(context)
                        : context.getManager().getTick() % 20 == 0;
@@ -153,6 +166,7 @@ public class SFMGameTestHelper extends GameTestHelper {
 
             @Override
             public void tick(ProgramContext context) {
+
                 if (!hasExecuted.get()) {
                     hasExecuted.set(true);
                     endTime.set(System.nanoTime());
@@ -161,6 +175,7 @@ public class SFMGameTestHelper extends GameTestHelper {
 
             @Override
             public Block getBlock() {
+
                 return new Block(Collections.emptyList());
             }
         };
@@ -168,24 +183,27 @@ public class SFMGameTestHelper extends GameTestHelper {
         triggers.add(0, startTimerTrigger);
         triggers.add(endTimerTrigger);
 
-        Runnable checker = () -> {
-            if (hasExecuted.get()) {
-                triggers.remove(startTimerTrigger);
-                triggers.remove(endTimerTrigger);
-                assertion.run();
-                SFMGameTestMethodHelpers.assertTrue(
-                        endTime.get() - startTime.get() < 80_000_000,
-                        "Program took too long to run: took " + NumberFormat
-                                .getInstance(Locale.getDefault())
-                                .format(endTime.get() - startTime.get()) + "ns"
-                );
-                hasExecuted.set(false); // prevent the assertion from running again
-                onSuccess.run();
-            }
-        };
-        LongStream
-                .range(getTick() + 1, timeoutTicks - getTick())
-                .forEach(i -> runAfterDelay(i, checker));
+        long bound = timeoutTicks - getTick();
+        for (long i = getTick() + 1; i < bound; i++) {
+            /// We cannot reuse the {@link Runnable} object because it is used as the key in a dict.
+            runAfterDelay(
+                    i, () -> {
+                        if (hasExecuted.get()) {
+                            triggers.remove(startTimerTrigger);
+                            triggers.remove(endTimerTrigger);
+                            assertion.run();
+                            SFMGameTestMethodHelpers.assertTrue(
+                                    endTime.get() - startTime.get() < 80_000_000,
+                                    "Program took too long to run: took " + NumberFormat
+                                            .getInstance(Locale.getDefault())
+                                            .format(endTime.get() - startTime.get()) + "ns"
+                            );
+                            hasExecuted.set(false); // prevent the assertion from running again
+                            onSuccess.run();
+                        }
+                    }
+            );
+        }
     }
 
     public void succeedIfManagerDidThingWithoutLagging(
@@ -203,4 +221,5 @@ public class SFMGameTestHelper extends GameTestHelper {
                 this::succeed
         );
     }
+
 }
