fdfe6c49c37608d520b8c097c2b5323f3835fa2c
TeamDman <TeamDman9201@gmail.com>
2025-11-09 15:53:57 -0500

    Fix side resolution logic for #380

--- src/main/java/ca/teamdman/sfml/ast/Side.java
+++ src/main/java/ca/teamdman/sfml/ast/Side.java
@@ -1,8 +1,8 @@
 package ca.teamdman.sfml.ast;
 
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.jetbrains.annotations.Nullable;
 
 public enum Side implements ASTNode {
@@ -19,8 +19,8 @@ public enum Side implements ASTNode {
     NULL;
 
 
-
     public static Side fromDirection(@Nullable Direction direction) {
+
         if (direction == null) return NULL;
         return switch (direction) {
             case UP -> TOP;
@@ -41,14 +41,21 @@ public enum Side implements ASTNode {
             case SOUTH -> Direction.SOUTH;
             case EAST -> Direction.EAST;
             case WEST -> Direction.WEST;
-            case LEFT -> blockState.getOptionalValue(DirectionalBlock.FACING)
-                    .map(Direction::getCounterClockWise).orElse(null);
-            case RIGHT -> blockState.getOptionalValue(DirectionalBlock.FACING)
-                    .map(Direction::getClockWise).orElse(null);
-            case FRONT -> blockState.getOptionalValue(DirectionalBlock.FACING)
+            case LEFT -> blockState.getOptionalValue(BlockStateProperties.HORIZONTAL_FACING)
+                    .or(() -> blockState.getOptionalValue(BlockStateProperties.FACING))
+                    .map(Direction::getClockWise)
                     .orElse(null);
-            case BACK -> blockState.getOptionalValue(DirectionalBlock.FACING)
-                    .map(Direction::getOpposite).orElse(null);
+            case RIGHT -> blockState.getOptionalValue(BlockStateProperties.HORIZONTAL_FACING)
+                    .or(() -> blockState.getOptionalValue(BlockStateProperties.FACING))
+                    .map(Direction::getCounterClockWise)
+                    .orElse(null);
+            case FRONT -> blockState.getOptionalValue(BlockStateProperties.HORIZONTAL_FACING)
+                    .or(() -> blockState.getOptionalValue(BlockStateProperties.FACING))
+                    .orElse(null);
+            case BACK -> blockState.getOptionalValue(BlockStateProperties.HORIZONTAL_FACING)
+                    .or(() -> blockState.getOptionalValue(BlockStateProperties.FACING))
+                    .map(Direction::getOpposite)
+                    .orElse(null);
             case NULL -> null;
         };
     }
