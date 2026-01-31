1a651bfc9e713759841c0a52d3aea86f38eed721
TeamDman <TeamDman9201@gmail.com>
2026-01-30 23:07:50 -0500

    Add resources, formatting and clippy rules

--- platform/cli/sfm-propagate-changes/src/lib.rs
+++ platform/cli/sfm-propagate-changes/src/lib.rs
@@ -23,6 +23,17 @@ pub fn main() -> eyre::Result<()> {
     // Initialize logging
     logging::init_logging(&cli.logging_config()?)?;
 
+    #[cfg(windows)]
+    {
+        // Enable ANSI support on Windows
+        // This fails in a pipe scenario, so we ignore the error
+        let _ = teamy_windows::console::enable_ansi_support();
+
+        // Warn if UTF-8 is not enabled on Windows
+        #[cfg(windows)]
+        teamy_windows::string::warn_if_utf8_not_enabled();
+    };
+    
     // Invoke whatever command was requested
     cli.invoke()
 }
