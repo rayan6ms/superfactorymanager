faea65725c740573835271a8f6d6bed4f81862af
TeamDman <TeamDman9201@gmail.com>
2026-02-12 20:30:09 -0500

    1.21.0 switch SFMMenuScreens to SFMSubscribeEvent

--- platform/minecraft/src/main/java/ca/teamdman/sfm/client/registry/SFMMenuScreens.java
+++ platform/minecraft/src/main/java/ca/teamdman/sfm/client/registry/SFMMenuScreens.java
@@ -1,16 +1,13 @@
 package ca.teamdman.sfm.client.registry;
 
-import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.client.screen.ManagerScreen;
 import ca.teamdman.sfm.client.screen.TestBarrelTankScreen;
+import ca.teamdman.sfm.common.event_bus.SFMSubscribeEvent;
 import ca.teamdman.sfm.common.registry.registration.SFMMenus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = SFM.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class SFMMenuScreens {
-    @SubscribeEvent
+    @SFMSubscribeEvent
     public static void register(RegisterMenuScreensEvent event) {
         event.register(SFMMenus.MANAGER.get(), ManagerScreen::new);
         event.register(SFMMenus.TEST_BARREL_TANK.get(), TestBarrelTankScreen::new);
